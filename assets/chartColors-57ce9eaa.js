1768ca16fc700b6542f19a7dd4af0ae2d3cb6547
Mateo Fuentes <mateof@gmail.com>
2025-04-20 13:30:20 +0200

    Updates

--- assets/chartColors-57ce9eaa.js
+++
@@ -1,1 +0,0 @@
-import{f as o}from"./index-08c872a2.js";const a=o({backgroundColor:"#42a5f5",borderColor:"#42a5f5"});export{a as c};
---
+++ assets/chartColors-791f905b.js
@@ -0,0 +1,1 @@
+import{f as o}from"./index-addb14f5.js";const a=o({backgroundColor:"#42a5f5",borderColor:"#42a5f5"});export{a as c};
